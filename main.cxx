5774327f085fd11d1ee1c179490b01bbc70770d1
DoanNguyenTrong <nguyentrongdoan.0@gmail.com>
2021-03-02 21:04:57 -0700

    disable hexagon delegate

--- main.cxx
+++ main.cxx
@@ -289,7 +289,7 @@ main(int argc, char const * argv[]) {
   std::cout << cv::getBuildInformation() << std::endl;
 
   // @doan 20210226: gstreamer "video/x-raw,format=NV12,framerate=30/1,width=1920,height=1080"
-  const char *pipeline = "qtiqmmfsrc ! video/x-raw,format=NV12,framerate=30/1,width=1920,height=1080 ! appsink";
+  const char *pipeline = "video/x-raw,format=NV12,framerate=30/1,width=1920,height=1080 ! appsink";
   // cv::VideoCapture cap(0);
   cv::VideoCapture cap(pipeline, cv::CAP_GSTREAMER);
   if (!cap.isOpened()) {
